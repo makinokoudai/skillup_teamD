82fb6d1139958d71350774abe1bcf6bc0ead1e42
RioTakahashi-6 <takahashi.rio.pw@mynavi.jp>
2023-05-26 06:31:33 +0000

    高橋

---
+++ Takahashi/tables_transport.py
@@ -0,0 +1,21 @@
+import sys
+from sqlalchemy import Column, String, Integer,Date
+from database import Base
+from database import ENGINE
+
+# テーブル：transportの定義
+class Transport(Base):
+    __tablename__='transport'
+    date = Column('date', Date, primary_key = True)
+    seq = Column('seq', Integer, primary_key = True)
+    departure = Column('departure', String(20))
+    arrival = Column('arrival', String(20))
+    via = Column('via', String(40))
+    amount = Column('amount', Integer)
+
+def main(args):
+    '''メイン関数'''
+    Base.metadata.create_all(bind=ENGINE)
+
+if __name__ == "__main__":
+    main(sys.argv)
