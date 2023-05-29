bdb8cff5957044f5c9a7aeef96ca15d944e19def
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-05-29 04:14:42 +0000

    Fix buy_drink.py by ma2mo10

--- ma2mo10/self-vend/src/MY_buy_drink.py
+++ ma2mo10/self-vend/src/MY_buy_drink.py
@@ -3,6 +3,7 @@ import MY_param as param
 from MY_input import input_YorN
 import MY_return_change as MYchange
 import MY_input as MYin
+import MY_display_item as MYdisp
 
 def calc_balance(item_name:str, money:int) -> int:
     '''購入後の残金を計算'''
@@ -15,7 +16,9 @@ def judge_buy(balance:int) -> bool:
 
 # 購入全体の処理
 def buy_drink(item_name:str, balance:int) -> None:
-    while True:
+    '''購入全体の処理'''
+    # item_nameが空ならループ終了
+    while item_name != '':
         # 残金の計算
         balance = calc_balance(item_name, balance)
         # 購入できるかの判定
@@ -24,6 +27,8 @@ def buy_drink(item_name:str, balance:int) -> None:
             print(f"残金：{balance}")
             # 購入を続けるかの入力受付
             if input_YorN() == 'Y':
+                # 商品の表示と商品名の入力受付
+                MYdisp.display_item()
                 item_name = MYin.input_item(balance)
             # Nが入力された場合お釣りを表示して終了
             else:
