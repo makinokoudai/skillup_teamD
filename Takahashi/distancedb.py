101f6b8e459a7bf5dca0f28bacd740f8a773fed8
RioTakahashi-6 <takahashi.rio.pw@mynavi.jp>
2023-05-29 02:50:22 +0000

    高橋

--- Takahashi/distancedb.py
+++ Takahashi/distancedb.py
@@ -4,15 +4,16 @@ from tables import Stations
 import sys
 
 args = sys.argv
+# 引数を変数sta1,sta2にそれぞれ代入
 sta1 = args[1]
 sta2 = args[2]
-
+# テーブルstationsにおいて、nameの値（sta1,sta2）を指定し、対応するkiloの値を抽出
 kilo1 = session.query(Stations.kilo).filter_by(name=sta1).first()
 kilo2 = session.query(Stations.kilo).filter_by(name=sta2).first()
-
-if kilo1[0] < kilo2[0]:
+# 駅間計算（絶対値で出力）
+if kilo1[0] < kilo2[0]: # kilo1-kilo2が負の値になるときの処理
     d = (kilo1[0] - kilo2[0]) * -1
-    print('{:.2f}'.format(d),end="")
+    print('{:.2f}'.format(d),end="") # 少数第二位までを出力
 else:
     d = kilo1[0] - kilo2[0]
     print('{:.2f}'.format(d),end="")
--- Takahashi/profitlunch.py
+++ Takahashi/profitlunch.py
@@ -1,18 +1,23 @@
 from decimal import Decimal, ROUND_HALF_UP
 import sys
 args = sys.argv
-
+# 引数を変数karaage,curryにそれぞれ代入
 karaage = int(args[1])
 curry = int(args[2])
 
-sales = karaage * 760 + curry * 850
+# 唐揚げ、カレーそれぞれの売り上げを計算
 sales_kareage = karaage * 760
-prime_karaage = sales_kareage * 0.323
 sales_curry = curry* 850
+# 唐揚げ、カレーそれぞれの原価を計算
+prime_karaage = sales_kareage * 0.323
 prime_curry = sales_curry * 0.284
-
+# 総売上を計算
+sales = karaage * 760 + curry * 850
+# 唐揚げ、カレーの原価の合計
 prime = prime_karaage + prime_curry
+# 原価の少数第一位を四捨五入
 demi_prime = Decimal(prime).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
+# 粗利の計算
 gross_profit = sales - demi_prime
-
+# 出力
 print("売上高："+str(sales)+"、原価："+str(demi_prime)+"、粗利："+str(gross_profit),end="")
