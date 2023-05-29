0871fe7787e56d9e89cacd1897fcbb1dd2808a23
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-05-29 02:40:45 +0000

    Fix vendcalc.py ma2mo10

--- ma2mo10/self-vend/src/MY_vendcalc.py
+++ ma2mo10/self-vend/src/MY_vendcalc.py
@@ -1,17 +1,18 @@
 import MY_display_item as MYdisp
 import MY_buy_drink as MYbuy
 
+
 def main():
     # 商品リストを表示
     MYdisp.display_item()
     # 投入金額と商品名の入力を受け取る
     money, item_name = MYdisp.set_item()
 
-    # item_nameが空ならcancelされたので終了
-    if item_name == []:
-        return 
-    
-    MYbuy
+    # item_nameが空ならcancelされたので終了、それ以外なら購入
+    if item_name == '':
+        return
+    else:
+        MYbuy.buy_drink(item_name, money)
 
 
 
