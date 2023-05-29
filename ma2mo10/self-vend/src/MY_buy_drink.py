3b954dde9a52b4492f8b85789ecc4ccf7c4b60d2
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-05-29 02:39:15 +0000

    Fix buy_drink.py by ma2mo10

--- ma2mo10/self-vend/src/MY_buy_drink.py
+++ ma2mo10/self-vend/src/MY_buy_drink.py
@@ -2,25 +2,32 @@
 import MY_param as param
 from MY_input import input_YorN
 import MY_return_change as MYchange
+import MY_input as MYin
 
 def calc_balance(item_name:str, money:int) -> int:
     '''購入後の残金を計算'''
     return money - param.const.item_dict[item_name]
 
+
 def judge_buy(balance:int) -> bool:
     '''残金で購入可能か判定'''
     return True if balance >= param.const.MIN_PRICE else False
 
-
-def buy_drink(money:int):
-    # 残金の計算
-    balance = calc_balance(money)
-    # 購入できるかの判定
-    if judge_buy(balance) == True:
-        # 残金を表示
-        print(f"残金：{balance}")
-        # 購入を続けるかの入力受付
-        input_YorN()
-    
-    else:
-        
+# 購入全体の処理
+def buy_drink(item_name:str, balance:int) -> None:
+    while True:
+        # 残金の計算
+        balance = calc_balance(item_name, balance)
+        # 購入できるかの判定
+        if judge_buy(balance) == True:
+            # 残金を表示
+            print(f"残金：{balance}")
+            # 購入を続けるかの入力受付
+            if input_YorN() == 'Y':
+                item_name = MYin.input_item(balance)
+            else:
+                MYchange.changedisplay(MYchange.changecalc(balance))
+                return
+        else:
+            MYchange.changedisplay(MYchange.changecalc(balance))
+            return 
